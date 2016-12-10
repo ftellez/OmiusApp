c22f7bbbc7e1761aaf69a439ffaa52332a72d36f
ftellez <fernando.saisd@gmail.com>
2016-12-09 21:36:55 -0600

    Set up of Omius app.

--- OmiusControl/app/src/main/java/com/omius/omiuscontrol/ControlView.java
+++ OmiusControl/app/src/main/java/com/omius/omiuscontrol/ControlView.java
@@ -1,19 +1,48 @@
 package com.omius.omiuscontrol;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
 public class ControlView extends AppCompatActivity {
+    private BluetoothAdapter mBtAdapter;
+    private BluetoothSocket btSocket;
+    private BluetoothServerSocket btSocketServer;
+    public String BtAddress = null;
+    private ConnectedThread mConnectedThread = null;
+    final int handlerState = 0;
+    // UUID service - This is the type of Bluetooth device that the BT module is
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    Handler bluetoothIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +51,7 @@ public class ControlView extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
@@ -52,5 +81,178 @@ public class ControlView extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (btSocket == null) {
+            ConnectDeviceBluetooth();
+        }
+    }
+
+    public void ConnectDeviceBluetooth() {
+        //It is best to check BT status at onResume in case something has changed while app was paused etc
+        checkBTState();
+
+        // Get a set of currently paired devices and append to pairedDevices list
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+
+        // Initialize array adapter for paired devices
+        final ArrayList<String> mPairedDevicesArrayAdapter = new ArrayList<String>();
+
+        // Add previously paired devices to the array
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+
+            final AlertDialog dialog;
+            final AlertDialog.Builder alertDialog = new AlertDialog.Builder(ControlView.this);
+            LayoutInflater inflater = getLayoutInflater();
+            View convertView = (View) inflater.inflate(R.layout.custom, null);
+            alertDialog.setView(convertView);
+            alertDialog.setTitle("Bluetooth Device List: ");
+            ListView lv = (ListView) convertView.findViewById(R.id.listView1);
+            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mPairedDevicesArrayAdapter);
+            lv.setAdapter(adapter);
+            dialog = alertDialog.show();
+
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                    Toast.makeText(getBaseContext(), "Connecting...", Toast.LENGTH_SHORT).show();
+                    // Get the device MAC address, which is the last 17 chars in the View
+                    String info = adapter.getItem(position);
+                    String address = info.substring(info.length() - 17);
+                    BtAddress = address;
+
+                    // Set up a pointer to the remote device using its address.
+                    BluetoothDevice device = mBtAdapter.getRemoteDevice(BtAddress);
+
+                    //Attempt to create a bluetooth socket for comms
+                    try {
+                        btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+//                        btSocketInsecure = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+                    } catch (IOException e1) {
+                        Toast.makeText(getBaseContext(), "ERROR - Could not create Bluetooth socket", Toast.LENGTH_SHORT).show();
+                    }
+
+                    ConnectSocket(btSocket);
+
+                    dialog.dismiss();
+                }
+            });
+        } else {
+            mPairedDevicesArrayAdapter.add("no devices paired");
+        }
+    }
+
+    public void ConnectSocket(BluetoothSocket BtSocket) {
+        // Establish the connection.
+//          btSocketServer = BtSocket;
+
+        try {
+            Toast.makeText(getBaseContext(), "Conectando....", Toast.LENGTH_SHORT).show();
+            BtSocket.connect();
+        } catch (IOException e) {
+            try {
+                BtSocket.close();        //If IO exception occurs attempt to close socket
+            } catch (IOException e2) {
+                Toast.makeText(getBaseContext(), "ERROR - Could not close Bluetooth socket", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        //When activity is resumed, attempt to send a piece of junk data ('x') so that it will fail if not connected
+        // i.e don't wait for a user to press button to recognise connection failure
+        if (mConnectedThread == null) {
+            mConnectedThread = new ConnectedThread(btSocket);
+            mConnectedThread.start();
+        }
+
+
+        //I send a character when resuming.beginning transmission to check device is connected
+        //If it is not an exception will be thrown in the write method and finish() will be called
+        //--------mConnectedThread.write("x");
+
+        final AlertDialog addressConf = new AlertDialog.Builder(ControlView.this)
+                .setTitle("Bluetooth Address")
+                .setMessage("Connection Ready!")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    //method to check if the device has Bluetooth and if it is on.
+    //Prompts the user to turn it on if it is off
+    private void checkBTState() {
+        // Check device has Bluetooth and that it is turned on
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter(); // CHECK THIS OUT THAT IT WORKS!!!
+        if (mBtAdapter == null) {
+            Toast.makeText(getBaseContext(), "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            if (!mBtAdapter.isEnabled()) {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    //create new class for connect thread
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        //creation of the connect thread
+        public ConnectedThread(BluetoothSocket ArrivingSocket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                //Create I/O streams for connection
+                tmpIn = ArrivingSocket.getInputStream();
+                tmpOut = ArrivingSocket.getOutputStream();
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "ERROR: I/O Streams Failed.", Toast.LENGTH_SHORT).show();
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            // Keep looping to listen for received messages
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);            //read bytes from input buffer
+                    String readMessage = new String(buffer, 0, bytes);
+                    // Send the obtained bytes to the UI Activity via handler
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        //write method
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+            } catch (IOException e) {
+                //if you cannot write, close the application
+                Toast.makeText(getBaseContext(), "Connection Failure", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 }
