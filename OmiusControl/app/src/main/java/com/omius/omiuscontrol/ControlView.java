3174fa9449038974bc2fadeaaf39c647b2e56c52
IngridNavarroA <ingrid.navarro.an@gmail.com>
2016-12-19 20:29:27 -0600

    n

--- OmiusControl/app/src/main/java/com/omius/omiuscontrol/ControlView.java
+++ OmiusControl/app/src/main/java/com/omius/omiuscontrol/ControlView.java
@@ -8,11 +8,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
